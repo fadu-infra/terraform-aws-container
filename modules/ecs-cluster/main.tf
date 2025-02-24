55df07889043a8d106d6d0a5baad5709dec28167
seoji kim <seoji.kim@fadutec.com>
2025-02-24 20:22:09 +0900

    refactor: move ECS security group configuration to main.tf

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -34,3 +34,33 @@ resource "aws_ecs_cluster_capacity_providers" "this" {
     }
   }
 }
+
+module "ecs_security_group" {
+  source = "git::git@gitlab.fadutec.dev:infra/devops/terraform-aws-network.git//modules/security-group?ref=feature/INFRAOPS-308"
+  # source = "app.terraform.io/fadutec/network/aws//modules/security-group"
+
+  name        = "${local.name}-sg"
+  description = "ECS nodes for ${local.name}"
+  vpc_id      = local.vpc_id
+  tags        = local.tags
+
+  ingress_rules = [
+    {
+      id         = "ingress_rule"
+      protocol   = "-1"
+      from_port  = 0
+      to_port    = 0
+      ipv4_cidrs = local.trusted_cidr_blocks
+    }
+  ]
+
+  egress_rules = [
+    {
+      id         = "egress_rule"
+      protocol   = "-1"
+      from_port  = 0
+      to_port    = 0
+      ipv4_cidrs = ["0.0.0.0/0"]
+    }
+  ]
+}
